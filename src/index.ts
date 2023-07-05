c69ac4a625b01f4483b7d30f1667b88edfea69cb
Amir Ziaei <amir.ziaei@zenitech.co.uk>
2023-07-05 17:12:35 +0300

    impl

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,66 @@
-import type { Handler } from '@netlify/functions'
+import type { Handler, HandlerEvent } from '@netlify/functions'
+import is_ip_private from 'private-ip'
 
-export const handler: Handler = async (event, context) => {
+const ERROR = {
+  MISSING_IP_ADDRESS: 'MISSING_IP_ADDRESS',
+  INVALID_IP_ADDRESS: 'INVALID_IP_ADDRESS',
+}
+
+type Response =
+  | {
+      status: 200
+      data?: {
+        ip: string
+        private: boolean
+      }
+    }
+  | {
+      status: 400
+      error: {
+        code: keyof typeof ERROR
+        message: string
+      }
+    }
+
+export const handler: Handler = async (event) => {
+  const { ip } = event.queryStringParameters ?? {}
+
+  if (!ip) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        status: 400,
+        error: {
+          code: 'MISSING_IP_ADDRESS',
+          message: 'Missing IP address. Make sure to include the `ip` query parameter.',
+        },
+      } satisfies Response),
+    }
+  }
+
+  const isPrivate = is_ip_private(ip)
+
+  if (typeof isPrivate !== 'boolean') {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        status: 400,
+        error: {
+          code: 'INVALID_IP_ADDRESS',
+          message: 'Invalid IP address. Make sure you are using a valid IPv4 or IPv6 address.',
+        },
+      } satisfies Response),
+    }
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    body: JSON.stringify({
+      status: 200,
+      data: {
+        ip,
+        private: isPrivate,
+      },
+    } satisfies Response),
   }
 }
